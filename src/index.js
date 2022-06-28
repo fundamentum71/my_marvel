f5695fa8e1b41213aa36e886d5d282f56e9868ad
Aleksandr <it.kucherov@gmail.com>
2022-06-28 12:19:13 +0300

    create randomChar and transform data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
-
-//создаем экземпляр класса
-const marvelService = new MarvelService();
-
-marvelService
-	.getAllCharacters()
-	.then((res) => res.data.results.forEach((item) => console.log(item.name)));
-//marvelService.getCharacter().then((res) => res.data.results);
 
 ReactDOM.render(
 	<React.StrictMode>
