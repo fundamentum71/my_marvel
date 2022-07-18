138d40f030699c3457fe8ab3dee79f19c6827569
Aleksandr <it.kucherov@gmail.com>
2022-07-18 16:06:33 +0300

    create http.hooj, changed marvelService and rendomChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
 
@@ -9,15 +9,11 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 const RandomChar = () => {
 	const [char, setChar] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(false);
 
-	//создаем экземпляр класса
-	const marvelService = new MarvelService();
+	const { loading, error, getCharacter } = useMarvelService();
 
 	const onCharLoaded = (char) => {
 		setChar(char);
-		setLoading(false);
 	};
 
 	useEffect(() => {
@@ -28,24 +24,14 @@ const RandomChar = () => {
 		};
 	}, []);
 
-	const onCharLoading = () => {
-		setLoading(true);
-	};
-
-	const onError = () => {
-		setLoading(false);
-		setError(true);
-	};
-
 	const updateChar = () => {
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-		onCharLoading();
-		marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+		getCharacter(id).then(onCharLoaded);
 	};
 
 	const errorMessage = error ? <ErrorMessage /> : null;
 	const spinner = loading ? <Spinner /> : null;
-	const content = !(loading || error) ? <View char={char} /> : null;
+	const content = !(loading || error || !char) ? <View char={char} /> : null;
 
 	return (
 		<div className="randomchar">
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,31 +1,24 @@
-class MarvelService {
-	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
-	_apiKey = 'apikey=2e77abbefd9c021797e5106c7312f34b';
-	_baseOffset = 210;
-	getResource = async (url) => {
-		let res = await fetch(url);
+import { useHttp } from '../hooks/http.hook';
+const useMarvelService = () => {
+	const { loading, request, error } = useHttp();
 
-		if (!res.ok) {
-			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-		}
+	const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	const _apiKey = 'apikey=2e77abbefd9c021797e5106c7312f34b';
+	const _baseOffset = 210;
 
-		return await res.json();
-	};
-
-	getAllCharacters = async (offset = this._baseOffset) => {
-		const res = await this.getResource(
-			`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(
+			`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
 		);
-		return res.data.results.map(this._transformCharacter);
-	};
-	getCharacter = async (id) => {
-		const res = await this.getResource(
-			`${this._apiBase}characters/${id}?${this._apiKey}`
-		);
-		return this._transformCharacter(res.data.results[0]);
+		return res.data.results.map(_transformCharacter);
 	};
 
-	_transformCharacter = (char) => {
+	const getCharacter = async (id) => {
+		const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+		return _transformCharacter(res.data.results[0]);
+	};
+
+	const _transformCharacter = (char) => {
 		return {
 			id: char.id,
 			name: char.name,
@@ -38,6 +31,7 @@ class MarvelService {
 			comics: char.comics.items,
 		};
 	};
-}
+	return { loading, error, getAllCharacters, getCharacter };
+};
 
-export default MarvelService;
+export default useMarvelService;
