5cbb2dcfd44c712310850dae2c5769db099016a3
Aleksandr <it.kucherov@gmail.com>
2022-07-19 11:55:17 +0300

    pagination

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,39 +1,13 @@
-import { useState } from 'react';
+import { MainPage, ComicsPage } from '../pages';
 import AppHeader from '../appHeader/AppHeader';
-import AppBanner from '../appBanner/AppBanner';
-import ComicsList from '../comicsList/ComicsList';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-	const [selectedChar, setChar] = useState(null);
-
-	const onCharSelected = (id) => {
-		setChar(id);
-	};
-
 	return (
 		<div className="app">
 			<AppHeader />
 			<main>
-				{/*<ErrorBoundary>
-					<RandomChar />
-				</ErrorBoundary>
-				<div className="char__content">
-					<ErrorBoundary>
-						<CharList onCharSelected={onCharSelected} />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<CharInfo charId={selectedChar} />
-					</ErrorBoundary>
-				</div>
-				<img className="bg-decoration" src={decoration} alt="vision" />*/}
-				<AppBanner />
-				<ComicsList />
+				<MainPage />
+				<ComicsPage />
 			</main>
 		</div>
 	);
